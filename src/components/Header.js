1d46e4a38557667a18d24b9b33d786ccf6daae05
ZZOMAN <reumia@gmail.com>
2017-10-24 17:01:58 +0900

    Header 스타일 추가, Variables 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = ({ profile, className }) => (
     <header className={ className }>
-        <h1 className="title">{ profile.name }</h1>
+        <h1 className="header-title">Resume</h1>
+        <div className="header-contents">
+            <div className="texts">
+                { profile.name }
+                { profile.email }
+            </div>
+            <div className="image">
+
+            </div>
+        </div>
     </header>
 )
 
