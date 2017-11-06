c46976e1aeae827cf19cc3e94077ae348ee90c8d
ZZOMAN <reumia@gmail.com>
2017-11-06 16:20:04 +0900

    Locale 한국어 수정

--- src/components/LocaleString.js
+++ src/components/LocaleString.js
@@ -10,7 +10,7 @@ const locales = {
     },
     'ko': {
         'companies': '경력',
-        'projects': '참여 프로젝트',
+        'projects': '대표 프로젝트',
         'personals': '개인 프로젝트',
         'company': '회사',
         'roles': '역할',
